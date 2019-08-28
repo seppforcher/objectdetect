79a2cf376a153b7b9ba83809e54816fa47c76fcf
Peter <ptummelt@ATVIENW9E571.ad-01.ent-01.adgroup>
2019-08-28 16:43:16 +0200

    refactored complete  code

--- objdetect.py
+++ objdetect.py
@@ -4,13 +4,13 @@ import numpy as np
 from multiprocessing import Process
 from multiprocessing import Queue
 import argparse
-from os.path import exists, isfile
+#from os.path import exists, isfile
 
 import edgetpu.detection.engine
-from edgetpu.utils import image_processing
+#from edgetpu.utils import image_processing
 from PIL import Image
 
-
+#cv2 capture (video or camera) 
 class MyCap:
     def __init__(self,filename, resolution=None):
         print('Trying to open Videofile ' + filename)
@@ -44,8 +44,8 @@ class MyCap:
         ret, frame =  self.cap.read()
         if ret == False:
             return None
-        resize = cv2.resize(frame, (self.frameWidth,self.frameHeight))
-        return Image.fromarray(resize)
+        return cv2.resize(frame, (self.frameWidth,self.frameHeight))
+        #Image.fromarray(resize)
         
         
 
@@ -97,7 +97,7 @@ class CoralDetector:
             if img is "EXIT":
                 return
                     
-            #shove it to CORAL USB stick (this is blocking)
+            #shove it to CORAL USB stick and wait for the results 
             results = self.engine.DetectWithImage(img, threshold=0.4, keep_aspect_ratio=True, relative_coord=False, top_k=10)
 
             data_out = []
@@ -130,135 +130,125 @@ class CoralDetector:
     def terminate(self):
         self.inputQueue.put("EXIT")                            
 
-#parse args
-parser = argparse.ArgumentParser()
-parser.add_argument('filename')
-parser.add_argument("-r", "--resolution", help="Video resolution widthxheigth (e.g. 640x480)")
-args = parser.parse_args()
 
+#display the resulting image with the detections
+class Frame:
+    def __init__(self):
+        self.confThreshold = 0.6
     
-#init video stream and detector
-cap = MyCap(args.filename, args.resolution)
-detector = CoralDetector()
-
-
-
+    def run(self, cap, detector):
+        #time the frame rate....
+        timer1 = time.time()
+        timer2 = 0
+        frames = 0
+        inferences = 0
+        fps = 0.0
+        qfps = 0.0
+        while(cap.isOpened()):
+            # Capture frame-by-frame
+            frame = cap.read()
+                
+            if frame:
+                if inferences == 1:
+                    timer2 = time.time()
         
-
-
-
-print("[INFO] starting capture...")
-
-#Misc vars
-fps = 0.0
-qfps = 0.0
-confThreshold = 0.6
-
-#time the frame rate....
-timer1 = time.time()
-frames = 0
-inferences = 0
-timer2 = 0
-
-while(cap.isOpened()):
-    # Capture frame-by-frame
-    img = cap.read()
+                #calssify current frame (non-blocking)
+                detector.put(Image.fromarray(frame))
+                #grab the detections (non-blocking)
+                out = detector.get()
         
-    if img:
-        if inferences ==1:
-            timer2 = time.time()
-
-        #calssify current frame (non-blocking)
-        detector.put(img)
-        # if the output queue *is not* empty, grab the detections
-        out = detector.get()
-
-        if out is not None:
-            # loop over the detections
-            for detection in out:
-                labeltxt = detection[0]
-                confidence = detection[1]
-                xmin = detection[2]
-                ymin = detection[3]
-                xmax = detection[4]
-                ymax = detection[5]
-                if confidence > confThreshold:
-                    #bounding box
-                    cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), color=(0, 255, 255))
-                    #label
-                    labLen = len(labeltxt)*5+40
-                    cv2.rectangle(frame, (xmin-1, ymin-1),\
-                    (xmin+labLen, ymin-10), (0,255,255), -1)
-                    #labeltext
-                    cv2.putText(frame,' '+labeltxt+' '+str(round(confidence,2)),\
-                    (xmin,ymin-2), cv2.FONT_HERSHEY_SIMPLEX, 0.3,(0,0,0),1,cv2.LINE_AA)
+                if out is not None:
+                    # loop over the detections
+                    for detection in out:
+                        labeltxt = detection[0]
+                        confidence = detection[1]
+                        xmin = detection[2]
+                        ymin = detection[3]
+                        xmax = detection[4]
+                        ymax = detection[5]
+                        if confidence > self.confThreshold:
+                            #bounding box
+                            cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), color=(0, 255, 255))
+                            #label
+                            labLen = len(labeltxt)*5+40
+                            cv2.rectangle(frame, (xmin-1, ymin-1), (xmin+labLen, ymin-10), (0,255,255), -1)
+                            #labeltext
+                            cv2.putText(frame,' '+labeltxt+' '+str(round(confidence,2)), (xmin,ymin-2), cv2.FONT_HERSHEY_SIMPLEX, 0.3,(0,0,0),1,cv2.LINE_AA)
+                
+                    inferences += 1
         
-            inferences += 1
-
-        vidpos = round(cap.get(cv2.CAP_PROP_POS_FRAMES) / cap.get(cv2.CAP_PROP_FRAME_COUNT) * 100)
-        # Display the resulting frame
-        cv2.rectangle(frame, (0,0), (cap.frameWidth,20), (0,0,0), -1)
-
-        cv2.rectangle(frame, (0,cap.frameHeight-20), (cap.frameWidth,cap.frameHeight), (0,0,0), -1)
-        cv2.putText(frame,'Threshold: '+str(round(confThreshold,1)), (10, 10), cv2.FONT_HERSHEY_SIMPLEX, 0.3,(0, 255, 255), 1, cv2.LINE_AA)
-
-        cv2.putText(frame,'VID FPS: '+str(fps), (cap.frameWidth-80, 10), cv2.FONT_HERSHEY_SIMPLEX, 0.3,(0, 255, 255), 1, cv2.LINE_AA)
-
-        cv2.putText(frame,'TPU FPS: '+str(qfps), (cap.frameWidth-80, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.3,(0, 255, 255), 1, cv2.LINE_AA)
-
-        cv2.putText(frame,'Video pos: '+str(vidpos), (10, cap.frameHeight-10), cv2.FONT_HERSHEY_SIMPLEX, 0.3,(0, 255, 255), 1, cv2.LINE_AA)
+                vidpos = round(cap.get(cv2.CAP_PROP_POS_FRAMES) / cap.get(cv2.CAP_PROP_FRAME_COUNT) * 100)
+                # Display the resulting frame
+                cv2.rectangle(frame, (0,0), (cap.frameWidth,20), (0,0,0), -1)
         
-
-        cv2.namedWindow('Coral',cv2.WINDOW_NORMAL)
-        cv2.resizeWindow('Coral',cap.frameWidth,cap.frameHeight)
-        cv2.imshow('Coral',frame)
+                cv2.rectangle(frame, (0,cap.frameHeight-20), (cap.frameWidth,cap.frameHeight), (0,0,0), -1)
+                cv2.putText(frame,'Threshold: '+str(round(self.confThreshold,1)), (10, 10), cv2.FONT_HERSHEY_SIMPLEX, 0.3,(0, 255, 255), 1, cv2.LINE_AA)
         
-        # FPS calculation
-        frames += 1
-        if frames >= 1:
-            end1 = time.time()
-            t1secs = end1-timer1
-            fps = round(frames/t1secs,2)
-        if inferences > 1:
-            end2 = time.time()
-            t2secs = end2-timer2
-            qfps = round(inferences/t2secs,2)
+                cv2.putText(frame,'VID FPS: '+str(fps), (cap.frameWidth-80, 10), cv2.FONT_HERSHEY_SIMPLEX, 0.3,(0, 255, 255), 1, cv2.LINE_AA)
+        
+                cv2.putText(frame,'TPU FPS: '+str(qfps), (cap.frameWidth-80, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.3,(0, 255, 255), 1, cv2.LINE_AA)
+        
+                cv2.putText(frame,'Video pos: '+str(vidpos), (10, cap.frameHeight-10), cv2.FONT_HERSHEY_SIMPLEX, 0.3,(0, 255, 255), 1, cv2.LINE_AA)
+                
+        
+                cv2.namedWindow('Coral',cv2.WINDOW_NORMAL)
+                cv2.resizeWindow('Coral',cap.frameWidth,cap.frameHeight)
+                cv2.imshow('Coral',frame)
+                
+                # FPS calculation
+                frames += 1
+                if frames >= 1:
+                    end1 = time.time()
+                    t1secs = end1-timer1
+                    fps = round(frames/t1secs,2)
+                if inferences > 1:
+                    end2 = time.time()
+                    t2secs = end2-timer2
+                    qfps = round(inferences/t2secs,2)
+        
+        
+                keyPress = cv2.waitKey(1) #Altering waitkey val can alter the framreate for vid files.
+                if keyPress == ord('q'):
+                    return
+                if keyPress == 82 and self.confThreshold < 1:
+                    self.confThreshold += 0.1
+                if keyPress == 84 and self.confThreshold > 0.4:
+                    self.confThreshold -= 0.1
+                if keyPress == 83:
+                    #print ("Curr pos: " + str(cap.get(cv2.CAP_PROP_POS_FRAMES)))
+                    framelapse = cap.get(cv2.CAP_PROP_FPS) *10 #10 seconds
+                    if cap.get(cv2.CAP_PROP_POS_FRAMES) + framelapse < cap.get(cv2.CAP_PROP_FRAME_COUNT):
+                        cap.set(cv2.CAP_PROP_POS_FRAMES, cap.get(cv2.CAP_PROP_POS_FRAMES) + framelapse)
+                    else:
+                        return
+                if keyPress == 81:
+                    #print ("Curr pos: " + str(cap.get(cv2.CAP_PROP_POS_FRAMES)))
+                    framelapse = cap.get(cv2.CAP_PROP_FPS) *10 #10 seconds
+                    if cap.get(cv2.CAP_PROP_POS_FRAMES) - framelapse > 0:
+                        cap.set(cv2.CAP_PROP_POS_FRAMES, cap.get(cv2.CAP_PROP_POS_FRAMES) - framelapse)
+                    else:
+                        cap.set(cv2.CAP_PROP_POS_FRAMES, 0)    
+                #print (str(keyPress))
+            else: 
+                return        
 
 
-        keyPress = cv2.waitKey(1) #Altering waitkey val can alter the framreate for vid files.
-        if keyPress == ord('q'):
-            break
-        if keyPress == 82:
-            confThreshold += 0.1
-        if keyPress == 84:
-            confThreshold -= 0.1
-        if confThreshold >1:
-            confThreshold = 1
-        if confThreshold <0.4:
-            confThreshold = 0.4
-        if keyPress == 83:
-            #print ("Curr pos: " + str(cap.get(cv2.CAP_PROP_POS_FRAMES)))
-            framelapse = cap.get(cv2.CAP_PROP_FPS) *10 #10 seconds
-            if cap.get(cv2.CAP_PROP_POS_FRAMES) + framelapse < cap.get(cv2.CAP_PROP_FRAME_COUNT):
-                cap.set(cv2.CAP_PROP_POS_FRAMES, cap.get(cv2.CAP_PROP_POS_FRAMES) + framelapse)
-            else:
-                break
-        if keyPress == 81:
-            #print ("Curr pos: " + str(cap.get(cv2.CAP_PROP_POS_FRAMES)))
-            framelapse = cap.get(cv2.CAP_PROP_FPS) *10 #10 seconds
-            if cap.get(cv2.CAP_PROP_POS_FRAMES) - framelapse > 0:
-                cap.set(cv2.CAP_PROP_POS_FRAMES, cap.get(cv2.CAP_PROP_POS_FRAMES) - framelapse)
-            else:
-                cap.set(cv2.CAP_PROP_POS_FRAMES, 0)    
-        #print (str(keyPress))
-    # Break the loop
-    else: 
-        break
-
-inputQueue.put("EXIT") 
-#Everything done, release the vid
-cap.cap.release()
-
-cv2.destroyAllWindows()
+if __name__== "__main__":
+    #parse args
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename')
+    parser.add_argument("-r", "--resolution", help="Video resolution widthxheigth (e.g. 640x480)")
+    args = parser.parse_args()
+    
+        
+    #init video stream, detector and frame display
+    cap = MyCap(args.filename, args.resolution)
+    detector = CoralDetector()
+    frame = Frame()
+    frame.run(cap, detector)
+    
+    detector.terminate()
+    cap.cap.release()
+    cv2.destroyAllWindows()
 
 
